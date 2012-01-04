461a97a92aeaebed813116dcbab9c2d48345766e
Quincy <rightabouthere@hotmail.ca>
2012-01-04 16:53:09 +0800

    an interface that probably all the classes will implement

---
+++ SpaceInvaders3D/src/spaceinvaders3d/Damageable.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spaceinvaders3d;
+
+/**
+ *
+ * @author quincy
+ */
+public interface Damageable {
+    void takeDamage(int n);
+    int getHP();
+}
