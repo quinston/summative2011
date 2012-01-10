bf1253f1cc524b38a7acfc968ac6eac384c8e307
unknown <308498617@.tdsb.on.ca>
2012-01-10 11:51:37 -0500

    added the corners to the Ship

--- SpaceInvaders3D/src/spaceinvaders3d/Ship.java
+++ SpaceInvaders3D/src/spaceinvaders3d/Ship.java
@@ -1,8 +1,19 @@
 package spaceinvaders3d;
 
 import spaceinvaders3d.Damageable;
+import spaceinvaders3d.Point3D;
 
 public class Ship implements Damageable {
+
+	private Point3D upperCorner, lowerCorner;
+	
+	public Point3D getUpperCorner() {
+		return upperCorner;
+	}
+	public Point3D getLowerCorner() {
+		return lowerCorner;
+	}
+
 	public Ship();
 	public void takeDamage(int n) {
 		hp -= n;
