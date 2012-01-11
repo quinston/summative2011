df4fb65afc7e75bf080bb091d7ffb4175c9a8eb1
unknown <308498617@.tdsb.on.ca>
2012-01-11 09:21:18 -0500

    indented

--- SpaceInvaders3D/src/spaceinvaders3d/Bullets.java
+++ SpaceInvaders3D/src/spaceinvaders3d/Bullets.java
@@ -3,32 +3,32 @@ import java.awt.*;
 
 public class Bullets implements damageable: 
 {
-    Point3d upperCorner, lowerCorner, direction;
-    
-    public Bullets(Point3d UpperCorner, Point3d LowerCorner, Point3d, movementDirection){
-	upperCorner = UpperCorner;
-	lowerCorner = LowerCorner;
-	direction = movementDirection;
-    }
-    public void move(){
-	upperCorner.x+=direction.x;
-	upperCorner.y+=direction.y;
-	upperCorner.z+=direction.z;
-	lowerCorner.x+=direction.x;
-	lowerCorner.y+=direction.y;
-	lowerCorner.z+=direction.z;
-    }
-    public boolean checkCollision(Point3d topLeft, Point3d botRight){
-	if((lowerCorner.x>=topLeft.x)
-	&&(upperCorner.x<=botRight.x)
-	&&(lowerCorner.y>=topLeft.y)
-	&&(upperCorner.y<=botRight.y)
-	&&(upperCorner.z>=botRight.z)
-	&&(lowerCorner.z<=topLeft.z)){
-	    return true;
+	Point3d upperCorner, lowerCorner, direction;
+	
+	public Bullets(Point3d UpperCorner, Point3d LowerCorner, Point3d, movementDirection){
+		upperCorner = UpperCorner;
+		lowerCorner = LowerCorner;
+		direction = movementDirection;
 	}
-	else{
-	    return false;
+	public void move(){
+		upperCorner.x+=direction.x;
+		upperCorner.y+=direction.y;
+		upperCorner.z+=direction.z;
+		lowerCorner.x+=direction.x;
+		lowerCorner.y+=direction.y;
+		lowerCorner.z+=direction.z;
 	}
-    }
+	public boolean checkCollision(Point3d topLeft, Point3d botRight){
+		if((lowerCorner.x>=topLeft.x)
+				&&(upperCorner.x<=botRight.x)
+				&&(lowerCorner.y>=topLeft.y)
+				&&(upperCorner.y<=botRight.y)
+				&&(upperCorner.z>=botRight.z)
+				&&(lowerCorner.z<=topLeft.z)){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
 } // Bullets class
